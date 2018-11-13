a2479a4eacec98b8356c350f429f8a78d5fd6179
CloverZxd <763866352@qq.com>
2018-11-13 14:12:29 +0800

    新增高德地图部分

---
+++ app/src/main/java/com/zxd/notconfused/ui/activity/MapActivity.java
@@ -0,0 +1,306 @@
+package com.zxd.notconfused.ui.activity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.LatLng;
+import com.zxd.notconfused.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MapActivity extends AppCompatActivity implements LocationSource, AMapLocationListener, View.OnClickListener {
+
+    @BindView(R.id.iv_back)
+    ImageView back;
+
+    @BindView(R.id.tv_title)
+    TextView  tv_title;
+
+    //展示地图
+    @BindView(R.id.map)
+    MapView mapView;
+    //AMap是地图对象
+    AMap aMap;
+    //声明AMapLocationClient类对象，定位发起端
+    private AMapLocationClient mLocationClient = null;
+    //声明mLocationOption对象，定位参数
+    public AMapLocationClientOption mLocationOption = null;
+    //声明mListener对象，定位监听器
+    private OnLocationChangedListener mListener = null;
+    //标识，用于判断是否只显示一次定位信息和用户重新定位
+    private boolean isFirstLoc = true;
+/*
+    //获取逆地址
+    GeocodeSearch geocodeSearch;
+    //逆向查询地址时需要传入的经纬度
+    LatLonPoint latLonPoint;*/
+
+    //卡片式布局用于显示定位的详细信息
+    @BindView(R.id.cardView)
+    CardView cardView;
+    //显示定位信息
+    @BindView(R.id.tv_address)
+    TextView tv_address;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        ButterKnife.bind(this);
+        back.setOnClickListener(this);
+        mapView.onCreate(savedInstanceState);
+        //判断定位相关权限打开了没有
+        List<String> permissionList = new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(MapActivity.this, Manifest.
+                permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(MapActivity.this, Manifest.
+                permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(MapActivity.this, Manifest.
+                permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if(!permissionList.isEmpty()){
+            String [] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MapActivity.this,permissions,1);
+        }else{
+            init();
+        }
+
+    }
+    private void init() {
+        if (aMap == null) {
+            // 显示地图
+            aMap = mapView.getMap();
+            //设置显示定位按钮 并且可以点击
+            UiSettings settings = aMap.getUiSettings();
+            aMap.setLocationSource(this);//设置了定位的监听
+            settings.setLogoBottomMargin(-50);//隐藏logo
+            settings.setZoomControlsEnabled(false);//隐藏放大缩小按钮
+            // 是否显示定位按钮
+            settings.setMyLocationButtonEnabled(true);
+            aMap.setMyLocationEnabled(true);//显示定位层并且可以触发定位,默认是flase
+        }
+
+        cardView.setCardElevation(8);//设置阴影部分大小
+        cardView.setContentPadding(5,5,5,5);//设置图片距离阴影大小
+
+
+        //开始定位
+        location();
+    }
+
+    private void location() {
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为Hight_Accuracy高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置是否只定位一次,默认为false
+        mLocationOption.setOnceLocation(false);
+        //设置是否强制刷新WIFI，默认为强制刷新
+        mLocationOption.setWifiActiveScan(true);
+        //设置是否允许模拟位置,默认为false，不允许模拟位置
+        mLocationOption.setMockEnable(false);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
+        mLocationClient.stopLocation();//停止定位
+        mLocationClient.onDestroy();//销毁定位客户端
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
+        mapView.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见官方定位类型表
+                aMapLocation.getLatitude();//获取纬度
+                aMapLocation.getLongitude();//获取经度
+                aMapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(aMapLocation.getTime());
+                df.format(date);//定位时间
+                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                aMapLocation.getCountry();//国家信息
+                aMapLocation.getProvince();//省信息
+                aMapLocation.getCity();//城市信息
+                aMapLocation.getDistrict();//城区信息
+                aMapLocation.getStreet();//街道信息
+                aMapLocation.getStreetNum();//街道门牌号信息
+                aMapLocation.getCityCode();//城市编码
+                aMapLocation.getAdCode();//地区编码
+
+                // 如果不设置标志位，此时再拖动地图时，它会不断将地图移动到当前的位置
+                if (isFirstLoc) {
+                    //设置缩放级别
+                    aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+                    //将地图移动到定位点
+                    aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude())));
+               /*     //进行逆地址查询
+                    latLonPoint = new LatLonPoint(aMapLocation.getLatitude(),aMapLocation.getLongitude());
+                    // 设置当前位置具体描述
+                    setCurrentLocationDetails();*/
+
+                    //点击定位按钮 能够将地图的中心移动到定位点
+                    mListener.onLocationChanged(aMapLocation);
+                    //添加图钉
+                    //  aMap.addMarker(getMarkerOptions(amapLocation));
+                    //获取定位信息
+                    StringBuffer buffer = new StringBuffer();
+                    buffer.append(aMapLocation.getCountry() + ""
+                            + aMapLocation.getProvince() + ""
+                            + aMapLocation.getCity() + ""
+                            + aMapLocation.getProvince() + ""
+                            + aMapLocation.getDistrict() + ""
+                            + aMapLocation.getStreet() + ""
+                            + aMapLocation.getStreetNum());
+//                    Toast.makeText(getApplicationContext(), buffer.toString(), Toast.LENGTH_LONG).show();
+                    //展示地址
+                    tv_title.setText(aMapLocation.getPoiName());
+                    tv_address.setText(aMapLocation.getAddress());
+                    isFirstLoc = false;
+                }
+
+
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+//                Toast.makeText(getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
+                tv_address.setText("获取详情地址信息失败");
+            }
+        }
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+    }
+
+    @Override
+    public void deactivate() {
+        mListener = null;
+    }
+
+    //设置打开定位权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0){
+                    for (int result:grantResults){
+                        if(result != PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(MapActivity.this,"必须同意所有权限才可以使用本应用",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    init();
+                }else{
+                    Toast.makeText(MapActivity.this,"发生未知错误",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_back:
+                finish();
+                break;
+        }
+    }
+/*
+    private void setCurrentLocationDetails(){
+        // 地址逆解析
+        geocodeSearch = new GeocodeSearch(getApplicationContext());
+        geocodeSearch.setOnGeocodeSearchListener(this);
+        // 第一个参数表示一个Latlng(经纬度)，第二参数表示范围多少米，第三个参数表示是火系坐标系还是GPS原生坐标系
+        RegeocodeQuery query = new RegeocodeQuery(latLonPoint, 25, GeocodeSearch.AMAP);
+        geocodeSearch.getFromLocationAsyn(query);
+
+    }*/
+
+
+/*    @Override
+    public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+        String formatAddress = regeocodeResult.getRegeocodeAddress().getFormatAddress();
+        Toast.makeText(MapActivity.this,formatAddress,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+    }*/
+}
--- app/src/main/java/com/zxd/notconfused/ui/fragment/homePageFragment.java
+++ app/src/main/java/com/zxd/notconfused/ui/fragment/homePageFragment.java
@@ -1,11 +1,19 @@
 package com.zxd.notconfused.ui.fragment;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,19 +24,47 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.bumptech.glide.Glide;
+import com.xuezj.cardbanner.CardBanner;
+import com.xuezj.cardbanner.ImageData;
+import com.xuezj.cardbanner.adapter.BannerAdapter;
+import com.xuezj.cardbanner.adapter.BannerViewHolder;
 import com.zxd.notconfused.R;
 import com.zxd.notconfused.adapter.homeGridViewAdpter;
 import com.zxd.notconfused.adapter.homeViewPagerAdapter;
 import com.zxd.notconfused.bean.a0;
+import com.zxd.notconfused.ui.activity.MapActivity;
+import com.zxd.notconfused.ui.views.bannerImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class homePageFragment extends Fragment{
+public class homePageFragment extends Fragment implements View.OnClickListener {
+
 
     //主View
     private  View view;
 
+    //====================获取地理位置信息=======================
+
+    //声明AMapLocationClient类对象
+    AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
+    //====================轮播图部分=============================
+    private CardBanner banner;
+    //轮播图展示的数据集合
+    private List<ImageData> imageData;
+    private ArrayList<String> image;
+
+
+
+    //====================菜单栏部分=============================
     //可滑动的viewpager
     private ViewPager vp;
 
@@ -51,14 +87,133 @@ public class homePageFragment extends Fragment{
     //小圆点图片的集合
     private ImageView[] ivPoints;
 
+    Toolbar toolbar;
+    //展示定位信息的布局
+    LinearLayout ll_location;
+    TextView tv_location;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_homepage,container,false);
+        toolbar=view.findViewById(R.id.toolbar);
+        //fragment声明
+        setHasOptionsMenu(true);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        //隐藏自带的项目名称
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        checkpermission();//获取定位相关权限
         initResources();//获取资源文件，动态添加列表数据
+        initBanner();
         initView();
         return view;
+    }
+
+    private void checkpermission() {
+        //判断定位相关权限打开了没有
+        List<String> permissionList = new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.
+                permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.
+                permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.
+                permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if(!permissionList.isEmpty()){
+            String [] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(getActivity(),permissions,1);
+        }else{
+            getCurrentLocationLatLng();//获取地理位置信息
+        }
+    }
+
+    private void getCurrentLocationLatLng() {
+        //展示定位信息
+        ll_location = view.findViewById(R.id.ll_location);
+        tv_location = view.findViewById(R.id.tv_location);
+        //默认显示定位中...
+        tv_location.setText("正在定位...");
+        ll_location.setOnClickListener(this);
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getActivity());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+
+ /* //设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景） 设置了场景就不用配置定位模式等
+    option.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
+    if(null != locationClient){
+        locationClient.setLocationOption(option);
+        //设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+        locationClient.stopLocation();
+        locationClient.startLocation();
+    }*/
+        // 同时使用网络定位和GPS定位,优先返回最高精度的定位结果,以及对应的地址描述信息
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //只会使用网络定位
+        /* mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);*/
+        //只使用GPS进行定位
+        /*mLocationOption.setLocationMode(AMapLocationMode.Device_Sensors);*/
+        // 设置为单次定位 默认为false
+        /*mLocationOption.setOnceLocation(true);*/
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。默认连续定位 切最低时间间隔为1000ms
+        mLocationOption.setInterval(10000);
+        //设置是否返回地址信息（默认返回地址信息）
+        /*mLocationOption.setNeedAddress(true);*/
+        //关闭缓存机制 默认开启 ，在高精度模式和低功耗模式下进行的网络定位结果均会生成本地缓存,不区分单次定位还是连续定位。GPS定位结果不会被缓存。
+        /*mLocationOption.setLocationCacheEnable(false);*/
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+
+    private void initBanner() {
+        banner = view.findViewById(R.id.banner);
+
+        image = new ArrayList<>();
+        image.add("http://ww1.sinaimg.cn/large/610dc034ly1fhyeyv5qwkj20u00u0q56.jpg");
+        image.add("https://ws1.sinaimg.cn/large/610dc034gy1fhvf13o2eoj20u011hjx6.jpg");
+        image.add("http://ww1.sinaimg.cn/large/610dc034ly1fhxe0hfzr0j20u011in1q.jpg");
+        banner.setBannerAdapter(new BannerAdapter() {
+            @Override
+            public int getCount() {
+                return image.size();
+            }
+
+            @Override
+            public BannerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                ViewHolder holder = new ViewHolder(LayoutInflater.from(getActivity())
+                        .inflate(R.layout.banner_item_homepage,parent,false));
+                return holder;
+            }
+
+            @Override
+            public void onBindViewHolder(BannerViewHolder holder, int position) {
+                ViewHolder viewHolder= (ViewHolder) holder;
+                Glide.with(getActivity())
+                        .load(image.get(position))
+                        .into(viewHolder.bannerImageView);
+            }
+        });
+
+        banner.start();
+        banner.setOnItemClickListener(new CardBanner.OnItemClickListener() {
+            @Override
+            public void onItem(int position) {
+                Toast.makeText(getActivity(), "position:" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 
@@ -121,7 +276,7 @@ public class homePageFragment extends Fragment{
             //循环加入圆点图片组
             ivPoints[i] = new ImageView(getActivity());
             if(i==0){//第一次初始化时默认第一个页面圆点为选中状态
-               ivPoints[i].setImageResource(R.drawable.check);
+                ivPoints[i].setImageResource(R.drawable.check);
             }else{
                 ivPoints[i].setImageResource(R.drawable.nocheck);
             }
@@ -164,6 +319,7 @@ public class homePageFragment extends Fragment{
 
 
     private void initResources() {
+
         for(int i=0;i<14;i++){
             a0 a0=new a0();
             a0.setImagName("测试"+i+">>");
@@ -212,4 +368,116 @@ public class homePageFragment extends Fragment{
             menuList.add(a0);
         }
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        banner.startAutoPlay();
+        if(mLocationClient!=null) {
+            mLocationClient.startLocation(); // 启动定位
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        banner.stopAutoPlay();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mLocationClient!=null) {
+            mLocationClient.stopLocation();//停止定位
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mLocationClient!=null) {
+            mLocationClient.onDestroy();//销毁定位客户端。
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case  R.id.ll_location:
+                startActivity(new Intent(getActivity(), MapActivity.class));
+                break;
+        }
+    }
+
+    class ViewHolder extends BannerViewHolder {
+        public bannerImageView bannerImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            bannerImageView = itemView.findViewById(R.id.item_img);
+        }
+    }
+
+    /**
+     * 定位回调监听器
+     */
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation amapLocation) {
+        /*    if (!IsGpsWork.isGpsEnabled(getActivity())) {
+                Toast toast = Toast.makeText(getActivity(), "打开gps", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            } else {*/
+                if (amapLocation != null) {
+                    if (amapLocation.getErrorCode() == 0) {
+                        //定位成功回调信息，设置相关消息
+                        amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                        double currentLat = amapLocation.getLatitude();//获取纬度
+                        double currentLon = amapLocation.getLongitude();//获取经度
+//                        latLonPoint = new LatLonPoint(currentLat, currentLon);  // latlng形式的
+                        /*currentLatLng = new LatLng(currentLat, currentLon);*/   //latlng形式的
+                        Log.i("currentLocation", "currentLat : " + currentLat + " currentLon : " + currentLon);
+                        amapLocation.getAccuracy();//获取精度信息
+                        tv_location.setText(amapLocation.getPoiName());
+                    } else {
+                        //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                        Log.e("AmapError", "location Error, ErrCode:"
+                                + amapLocation.getErrorCode() + ", errInfo:"
+                                + amapLocation.getErrorInfo());
+                        tv_location.setText("定位失败");
+                    }
+                }
+            //}
+        }
+    };
+
+    //设置打开定位权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0){
+                    for (int result:grantResults){
+                        if(result != PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(getActivity(),"必须同意所有权限才可以使用本应用",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                    getCurrentLocationLatLng();//获取地理位置信息
+                }else{
+                    Toast.makeText(getActivity(),"发生未知错误",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+/*    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        if (getChildFragmentManager().getBackStackEntryCount() == 0) {
+            inflater.inflate(R.menu.menu_home_page_fragment, menu);
+        }
+    }*/
 }
